6626372f7230765dbeb6a030f3d25a9aff2aa8c8
Gustavo Kozlowski <gustavokozlowski96@gmail.com>
2022-03-10 11:31:33 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,5 +1,5 @@
-import {ReactComponent as Arrow} from 'assets/img/arrow.svg'
-import './styles.css'
+import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
+import './styles.css';
 export default function Pagination(){
     return(
         <div className="dsmovie-pagination-container">
